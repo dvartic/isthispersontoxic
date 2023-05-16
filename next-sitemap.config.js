73822e5f277f555cfac4efc3bd620cf160b097ff
dvartic <dvartic@users.noreply.github.com>
2023-05-16 19:09:43 +0200

    Exclude report URLs from sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,6 @@ module.exports = {
     siteUrl: process.env.BASE_PATH || "https://edepo-web.vercel.app",
     generateRobotsTxt: true, // (optional)
     generateIndexSitemap: false,
+    exclude: ["/report", "/report/*"],
     // ...other options
 };
